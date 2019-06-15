2331d413e82a640104d24305a3de8df0302dcd8e
AnaSanchezS <33376532+AnaSanchezS@users.noreply.github.com>
2019-06-15 15:02:59 -0500

    Update TestAutomatedAlarm.py

--- TestAutomatedAlarm.py
+++ TestAutomatedAlarm.py
@@ -4,3 +4,19 @@ x = 1
 if x == 1:
     # indented four spaces
     print("x is 1.")
+
+    
+    #Importing the modules
+import urllib2
+import json
+
+screen_name = "wordpress"
+
+url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + screen_name
+
+data = json.load(urllib2.urlopen(url))
+
+print len(data), "tweets"
+
+for tweet in data:
+    print tweet['text']
